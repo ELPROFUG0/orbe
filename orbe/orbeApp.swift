044df6e2fa1884f8a244d47ac1d6e1fde56bad08
Moises <157192265+elprofugo@users.noreply.github.com>
2026-01-20 12:52:46 -0600

    Initial Commit

---
+++ orbe/orbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  orbeApp.swift
+//  orbe
+//
+//  Created by content manager on 20/01/26.
+//
+
+import SwiftUI
+
+@main
+struct orbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ orbeTests/orbeTests.swift
@@ -0,0 +1,17 @@
+//
+//  orbeTests.swift
+//  orbeTests
+//
+//  Created by content manager on 20/01/26.
+//
+
+import Testing
+@testable import orbe
+
+struct orbeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
